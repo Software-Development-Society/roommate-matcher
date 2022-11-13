80a96ea5b00552529b6c6cd1109f3b6a90686655
Holden Cormier <hcormie2@villanova.edu>
2022-11-12 23:00:35 -0500

    fixing some main issues

--- matcher-app/app.js
+++ matcher-app/app.js
@@ -71,7 +71,7 @@ app.get("/login", function (req, res) {
 });
 
 app.get("/register", function (req, res) {
-    res.sendFile(__dirname + "/public/register.html");
+    res.sendFile(__dirname + "/public/register/register.html");
 });
 
 
