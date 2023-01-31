8bfb206289a89b5740f87bcb4faa4c14fea04f39
jjohn49 <98774069+jjohn49@users.noreply.github.com>
2023-01-31 13:21:03 -0500

    created the method that will make the form json for the backend

--- matcher-app/routes/upload-form.js
+++ matcher-app/routes/upload-form.js
@@ -1,15 +1,73 @@
 const express = require('express');
 const router = express.Router();
 
+
+
 router.post("/upload-form", (req, res) => {
+    //All questions answered will make a 66 entry long json file
+    //if there is less than 66 keys we know that not all questions were answered properly
     if(Object.keys(req.body).length < 66){
         //Not all questions were answered
         console.log("Questions unanswered")
+    //console.log(req.body)
+    
     }else{
-        //api method to send to backend
+        let json = makeFormJsonToSendToBackend(req) 
         res.redirect('/dashboard')
     }
-    console.log(req.body);
+    
 })
+
+
+//method takes the request and creates JSON to send to the backend
+function makeFormJsonToSendToBackend(req){
+    var json = {}
+    json["user_id"] = req.user.id
+        json["sex"] = req.user.sex
+        json["responses"] = []
+
+
+        let keys = Object.keys(req.body)
+        console.log(keys.length)
+
+        //looping through the json received from form.ejs and assigning it to vars to make more readable
+        for(var x = 0; x < keys.length; x+=3){
+            let answer = req.body[keys[x]]
+            let numOfAnswers = req.body[keys[x + 1]]
+            let roommateAnswer = req.body[keys[x+2]]
+
+            json["responses"].push([getQuestionValue(answer,numOfAnswers), getRoommateValue(roommateAnswer)])
+        }
+        console.log(json)
+}
+
+//Below Methods here to convert the values from the form to the values we are using in the backend
+//Ex: if there are only two possible answers, then the form will give the node js becakend either 1 or 2
+//but we want it to be either 1 or 5
+function getQuestionValue(answer, numOfAnswers){
+    if(numOfAnswers == '2' && answer == '2'){
+        answer = '5'
+    }else if (numOfAnswers == '3'){
+        if(answer == '2'){
+            answer = '3'
+        }else if(answer == '3'){
+            answer = '5'
+        }
+    }
+    return answer
+}
+
+//I know these are hard coded and all but they are good enough
+function getRoommateValue(roommateAnswer){
+    if(roommateAnswer == '3'){
+        roommateAnswer = '4'
+    }else if(roommateAnswer == '4'){
+        roommateAnswer = '5'
+    }
+
+    return roommateAnswer
+}
+
+
 
 module.exports = router
