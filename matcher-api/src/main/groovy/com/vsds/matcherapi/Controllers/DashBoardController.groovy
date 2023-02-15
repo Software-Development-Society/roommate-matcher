d28b8cecfc97e4b976bc858d65c4b23ab21b37d6
Holden Cormier <hcormie2@villanova.edu>
2023-02-14 19:15:43 -0500

    fixing array

--- matcher-api/src/main/groovy/com/vsds/matcherapi/Controllers/DashBoardController.groovy
+++ matcher-api/src/main/groovy/com/vsds/matcherapi/Controllers/DashBoardController.groovy
@@ -27,12 +27,26 @@ class DashBoardController {
 
         MatchList currentUserMatches = DatabaseServices.getMatches(user_id)
         Map<ObjectId, Integer> matches = currentUserMatches.getMatchList()
-        ArrayList<String> resultList = new ArrayList<>()
+        ArrayList<JSONObject> resultList = new ArrayList<>()
 
         for (ObjectId match_id : matches.keySet()){
             Users currentUser = DatabaseServices.getUserFromId(match_id)
             if (currentUser.getSex() == DatabaseServices.getUserFromId(user_id).getSex() && currentUser.getUserId() != DatabaseServices.getUserFromId(user_id).getUserId()){
-                resultList.add(currentUser as String)
+                JSONObject currentUserObject = new JSONObject()
+                currentUserObject.put("user_id", currentUser.getUserId())
+                currentUserObject.put("first_name", currentUser.getFirstName())
+                currentUserObject.put("last_name", currentUser.getLastName())
+                currentUserObject.put("username",currentUser.getUsername())
+                currentUserObject.put("sex", currentUser.getSex())
+                currentUserObject.put("class_year", currentUser.getClassYear())
+                currentUserObject.put("bio", currentUser.getBio())
+                currentUserObject.put("picture_name", currentUser.getPictureName())
+                currentUserObject.put("instagram", currentUser.getInstagram())
+                currentUserObject.put("snap", currentUser.getSnapChat())
+
+
+                resultList.add(currentUserObject)
+
             }
 
         }
